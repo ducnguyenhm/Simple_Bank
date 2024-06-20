a3bdc312a9aacffccab8337c1c832d6cbb54bde4
Đức Nguyễn <ducnguyen@MacBook-Pro-cua-uc.local>
2024-06-21 01:18:13 +0700

    Implement RESRful API with GIN and Load Config file with viper

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	db "simple_bank/db/sqlc"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	router.POST("/accounts", server.CreateAccount)
+	router.GET("/accounts/:id", server.GetAccount)
+	router.GET("/accounts", server.ListAccount)
+
+	//add routes to router
+
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
